6e55594a6c42c1a9c363544f284c903074d586cd
EliasOMacias <167808478+EliasOMacias@users.noreply.github.com>
2024-06-29 11:35:14 -0700

    Wrote to do list in Main

--- Main.py
+++ Main.py
@@ -1,3 +1,26 @@
 from menu import Menu, MenuItem
 from coffe_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+#todo 1: Prompt user by asking what would you like?
+    #todo 1a: check users input
+    #todo 2a: prompt should show after each completed action
+
+#todo 2: turn off the coffee machine
+
+#todo 3: print report which shows current resources
+
+#todo 4: check resources sufficient
+    #todo 4a: when the user chooses a drink the program should chick if there are enough resources
+    #todo 4b: if not report what is insufficient
+
+#todo 5: process coins
+    #todo 5a: if there are sufficient resources, program should prompt user to insert coins.
+    #todo 5b: calculate inserted value
+
+#todo 6: check if transaction successful
+    #todo 6a: check if funds are sufficient after coin counting operation
+    #todo 6b: if successful add money to profit to be reflected in report
+    #todo 6c: offer change
+def CoffeeMachine()
