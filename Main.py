b3a90d146e239e6c67eeba2f8bb9678c59e0d36a
EliasOMacias <167808478+EliasOMacias@users.noreply.github.com>
2024-06-29 12:44:59 -0700

    All to do's completed let's rock

--- Main.py
+++ Main.py
@@ -6,25 +6,11 @@ money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
 
-#todo 1: Prompt user by asking what would you like?
-    #todo 1a: check users input
-    #todo 2a: prompt should show after each completed action
 
 
 
 
 
-#todo 4: check resources sufficient
-    #todo 4b: if not report what is insufficient
-
-#todo 5: process coins
-    #todo 5a: if there are sufficient resources, program should prompt user to insert coins.
-    #todo 5b: calculate inserted value
-
-#todo 6: check if transaction successful
-    #todo 6a: check if funds are sufficient after coin counting operation
-    #todo 6b: if successful add money to profit to be reflected in report
-    #todo 6c: offer change
 
 
 
@@ -40,7 +26,8 @@ while is_on:
         is_on = False
     else:
         drink = menu.find_drink(choice)
-        coffee_maker.is_resource_sufficient(drink):
-        if
+        if coffee_maker.is_resource_sufficient(drink):
+            money_machine.make_payment(drink.cost)
+            coffee_maker.make_coffee(drink)
 
 
