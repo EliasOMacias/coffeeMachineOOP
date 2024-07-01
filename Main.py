489fe621fad2395b6e7cb18b7413d7245da34ff1
EliasOMacias <167808478+EliasOMacias@users.noreply.github.com>
2024-06-30 23:27:33 -0700

    fixed issue where an entry not on the menu would cause the app to exit in error

--- Main.py
+++ Main.py
@@ -12,13 +12,19 @@ menu = Menu()
 
 
 
-
+print(menu.get_items())
 
 
 is_on = True
 while is_on:
     options = menu.get_items()
-    choice = input(f'What would you like ({options})\n')
+    invalid_choice = True
+    while invalid_choice:
+        choice = input(f'What would you like ({options})\n')
+        if choice.lower() not in ['latte', 'cappuccino', 'espresso', 'report', 'off']:
+            print('sorry, try again')
+        else:
+            invalid_choice = False
     if choice.lower() == 'report':
         coffee_maker.report()
         money_machine.report()
@@ -26,8 +32,8 @@ while is_on:
         is_on = False
     else:
         drink = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink):
-            money_machine.make_payment(drink.cost)
-            coffee_maker.make_coffee(drink)
 
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
